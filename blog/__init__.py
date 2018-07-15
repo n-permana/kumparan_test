e7ac2951b2d652a7922e237cc0a07c22db51dae9
Nandang <permana0415@gmail.com>
2018-07-15 22:20:19 +0700

    change config Mongodb

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+import os
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/kumparan_test"
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/kumparan_test"
+app.config["MONGO_URI"] = MONGO_URL
 mongo = PyMongo(app)
 news_collection = mongo.db.news
 
